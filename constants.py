36ef209265fd08529bbb6ea14786d51d91dce1c1
Yuni <fuchioka@cs.ubc.ca>
2022-01-05 20:06:22 -0800

    checked and updated orobot physical parameters

--- constants.py
+++ constants.py
@@ -2,10 +2,17 @@ import numpy as np
 import enum
 
 
-l_Bx = 0.356
-l_By = 0.2
-l_thigh = 0.16
-l_calf = 0.16
+l_Bx = 0.380  # length of body, measured axis to axis at hip (from CAD)
+# width between left and right feet (from CAD)
+# outer face to face was 310mm, inner face to face was 290mm, thickness of lower leg is 10mm
+l_By = 0.3
+l_thigh = 0.165  # length of upper leg module measured axis to axis (from CAD)
+l_calf = 0.160  # length of lower leg measured axis to axis (from CAD)
+
+# TODO: check if mass of motors is included and below inertial properties
+m = 1.43315091  # mass of body  (from URDF)
+# body moment of inertia in body frame (from URDF)
+B_I = np.diag([0.00578574, 0.01938108, 0.02476124])
 
 
 # enum for the four legs
