bed5e940d6b0570eef92badce06e2b5028c7884a
Yuni <fuchioka@cs.ubc.ca>
2022-01-12 19:17:15 -0800

    refactor stand, walk, jump motions using helper functions

--- generate_reference.py
+++ generate_reference.py
@@ -9,6 +9,7 @@ from utils import (
     homog_np,
 )
 import numpy as np
+from numpy import pi
 from scipy.interpolate import interp1d
 from scipy.interpolate import CubicHermiteSpline
 
@@ -29,12 +30,22 @@ def cubic_interp_t(x, y, t):
 
 # sinusoidal function evaluated at t defined using oscillation period, minimum
 # and maximum values
-def sinusoid(period, min_val, max_val, t):
-    return (max_val - min_val) / 2.0 * (1 - np.cos(2 * np.pi / period * t)) + min_val
+def sinusoid(period, min_val, max_val, t, phase_offset=0):
+    return (max_val - min_val) / 2.0 * (
+        1 - np.cos(2 * np.pi / period * t + phase_offset)
+    ) + min_val
 
 
 def generate_reference():
-    tf = 5.0
+    motion_type = "stand"
+
+    if motion_type == "walk":
+        tf = 5.0
+    if motion_type == "jump":
+        tf = 2.0
+    elif motion_type == "stand":
+        tf = 5.0
+
     N = int(tf * 20)
     dt = tf / (N)
     t_vals = np.linspace(0, tf, N + 1)
@@ -43,25 +54,55 @@ def generate_reference():
     U = np.zeros((24, N + 1))
 
     for k in range(N + 1):
-        angle = cubic_interp_t(
-            [0, 1.6, 2.5, 3.4, tf], [0, 0, np.pi / 2.0, 0, 0], t_vals[k]
-        )
-        p = np.array([-l_Bx / 2.0, 0.0, l_thigh])
-        p_xz = rot_mat_2d_np(angle) @ np.array([l_Bx / 2.0, 0.0])
-        p += np.array([p_xz[0], 0.0, p_xz[1]])
-        R = rot_mat_np(np.array([0.0, 1.0, 0.0]), -angle)
+        if motion_type == "walk":
+            body_x = sinusoid(period=2.5, min_val=-0.3, max_val=0.3, t=t_vals[k])
+            p = np.array([body_x, 0.0, 0.2])
+            R = np.eye(3)
+            p_i = {}
+            for leg in legs:
+                p_i[leg] = B_p_Bi[leg].copy()
+                p_i[leg][0] += body_x
+                if leg == legs.FL or leg == legs.HR:
+                    p_i[leg][2] += max(0.0, sinusoid(0.6, -0.05, 0.05, t_vals[k], 0.0))
+                else:
+                    p_i[leg][2] += max(0.0, sinusoid(0.6, -0.05, 0.05, t_vals[k], pi))
+        if motion_type == "jump":
+            t_apex = 0.3
+            z_apex = np.linalg.norm(g) * t_apex ** 2 / 2.0
+            body_z = cubic_interp_t(
+                [0, 0.2 * tf, 0.2 * tf + t_apex, 0.2 * tf + 2 * t_apex, tf],
+                [0, 0, z_apex, 0, 0],
+                t_vals[k],
+            )
+            p = np.array([0.0, 0.0, 0.2])
+            p[2] += body_z
+            R = np.eye(3)
+            p_i = {}
+            for leg in legs:
+                p_i[leg] = B_p_Bi[leg].copy()
+                p_i[leg][2] += body_z
+        elif motion_type == "stand":
+            angle = cubic_interp_t(
+                [0, 1.6, 2.5, 3.4, tf], [0, 0, np.pi / 2.0, 0, 0], t_vals[k]
+            )
+            p = np.array([-l_Bx / 2.0, 0.0, l_thigh])
+            p_xz = rot_mat_2d_np(angle) @ np.array([l_Bx / 2.0, 0.0])
+            p += np.array([p_xz[0], 0.0, p_xz[1]])
+            R = rot_mat_np(np.array([0.0, 1.0, 0.0]), -angle)
+            p_i = {}
+            T_B = homog_np(p, R)
+            for leg in legs:
+                if leg == legs.FL or leg == legs.FR:
+                    p_Bi = mult_homog_point_np(T_B, B_p_Bi[leg])
+                    p_i[leg] = p_Bi.copy()
+                    p_i[leg][2] -= l_thigh
+                else:
+                    p_i[leg] = B_p_Bi[leg].copy()
+
         pdot = np.array([0.0, 0.0, 0.0])
         omega = np.array([0.0, 0.0, 0.0])
-        p_i = {}
         f_i = {}
-        T_B = homog_np(p, R)
         for leg in legs:
-            if leg == legs.FL or leg == legs.FR:
-                p_Bi = mult_homog_point_np(T_B, B_p_Bi[leg])
-                p_i[leg] = p_Bi.copy()
-                p_i[leg][2] -= l_thigh
-            else:
-                p_i[leg] = B_p_Bi[leg].copy()
             f_i[leg] = np.array([0.0, 0.0, 0.0])
             if p_i[leg][2] <= eps:
                 f_i[leg][2] = m * np.linalg.norm(g) / 4.0
