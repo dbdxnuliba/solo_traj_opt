78ab9e6312a04f9d16074fab3287f2052f837a70
Yuni Fuchioka <fuchioka@cs.ubc.ca>
2022-05-19 14:31:37 -0700

    force qp mostly working but with some foot switching bug

--- force_qp.py
+++ force_qp.py
@@ -3,6 +3,7 @@ import scipy.sparse as sp
 import osqp
 
 from constants import *
+from draw import animate_traj
 from utils import extract_state_np, skew_sp, extract_X_from_XR
 from generate_reference import generate_reference
 
@@ -110,15 +111,15 @@ def solve_force_qp(X_cqp, X_ref, dt):
     # construct objective
     P = sp.lil_matrix((dim_x_fqp * (N + 1), dim_x_fqp * (N + 1)))
     q = np.zeros(dim_x_fqp * (N + 1))
-    for t in np.arange(N + 1):
+    for idx, t in enumerate(np.arange(N + 1)):
         r_cqp_t, l_cqp_t, k_cqp_t, p_i_cqp_t, f_i_cqp_t = extract_state_np(X_cqp, t)
         r_ref_t, l_ref_t, k_ref_t, p_i_ref_t, f_i_ref_t = extract_state_np(X_ref, t)
         P_t, q_t = calc_obj_t(r_ref_t, l_ref_t, k_ref_t, r_cqp_t, l_cqp_t, k_cqp_t)
 
-        row_start = dim_x_fqp * t
-        row_end = dim_x_fqp * (t + 1)
-        col_start = dim_x_fqp * t
-        col_end = dim_x_fqp * (t + 1)
+        row_start = dim_x_fqp * idx
+        row_end = dim_x_fqp * (idx + 1)
+        col_start = dim_x_fqp * idx
+        col_end = dim_x_fqp * (idx + 1)
 
         P[row_start:row_end, col_start:col_end] = P_t
         q[row_start:row_end] = q_t
@@ -133,7 +134,6 @@ def solve_force_qp(X_cqp, X_ref, dt):
     # dynamics
     for idx, t in enumerate(np.arange(1, N + 1)):
         r_cqp_t, l_cqp_t, k_cqp_t, p_i_cqp_t, f_i_cqp_t = extract_state_np(X_cqp, t)
-        r_ref_t, l_ref_t, k_ref_t, p_i_ref_t, f_i_ref_t = extract_state_np(X_ref, t)
         P_t, q_t = calc_obj_t(r_ref_t, l_ref_t, k_ref_t, r_cqp_t, l_cqp_t, k_cqp_t)
         l_i_cqp_t = {}
         for leg in legs:
@@ -149,6 +149,53 @@ def solve_force_qp(X_cqp, X_ref, dt):
         l[row_start:row_end] = l_dyn_t
         u[row_start:row_end] = u_dyn_t
 
+    # friction
+    for idx, t in enumerate(np.arange(N + 1)):
+        r_cqp_t, l_cqp_t, k_cqp_t, p_i_cqp_t, f_i_cqp_t = extract_state_np(X_cqp, t)
+        c_i_cqp_t = {}
+        for leg in legs:
+            c_i_cqp_t[leg] = p_i_cqp_t[leg][-1] < eps_contact
+        A_fric_t, l_fric_t, u_fric_t = calc_fric_t(c_i_cqp_t)
+
+        row_start = dim_dyn_fqp * N + dim_fric_fqp * idx
+        row_end = dim_dyn_fqp * N + dim_fric_fqp * (idx + 1)
+        col_start = dim_x_fqp * idx
+        col_end = dim_x_fqp * (idx + 1)
+
+        A[row_start:row_end, col_start:col_end] = A_fric_t
+        l[row_start:row_end] = l_fric_t
+        u[row_start:row_end] = u_fric_t
+
+    # kinematics
+    for idx, t in enumerate(np.arange(N + 1)):
+        r_cqp_t, l_cqp_t, k_cqp_t, p_i_cqp_t, f_i_cqp_t = extract_state_np(X_cqp, t)
+        A_kin_t, l_kin_t, u_kin_t = calc_kin_t(p_i_cqp_t)
+
+        row_start = dim_dyn_fqp * N + dim_fric_fqp * (N + 1) + dim_kin_fqp * idx
+        row_end = dim_dyn_fqp * N + dim_fric_fqp * (N + 1) + dim_kin_fqp * (idx + 1)
+        col_start = dim_x_fqp * idx
+        col_end = dim_x_fqp * (idx + 1)
+
+        A[row_start:row_end, col_start:col_end] = A_kin_t
+        l[row_start:row_end] = l_kin_t
+        u[row_start:row_end] = u_kin_t
+
+    # set up QP and solve
+    qp = osqp.OSQP()
+    qp.setup(P=P.tocsc(), q=q, A=A.tocsc(), l=l, u=u, **osqp_settings)
+    results = qp.solve()
+
+    X_sol_fqp = results.x.reshape((dim_x_fqp, N + 1), order="F")
+
+    X_sol = np.empty((dim_x, N + 1))
+    X_sol[:9, :] = X_sol_fqp[:9, :]  # r, l, k
+    X_sol[9:21, :] = X_cqp[9:21, :]  # p_i
+    X_sol[21:, :] = X_sol_fqp[9:, :]  # f_i
+
+    info = results.info
+
+    return X_sol, info
+
 
 # test functions
 if __name__ == "__main__":
@@ -156,7 +203,13 @@ if __name__ == "__main__":
     X_ref = extract_X_from_XR(XR_ref)
     X_cqp = X_ref
 
-    solve_force_qp(X_cqp, X_ref, dt)
+    X_sol, info = solve_force_qp(X_cqp, X_ref, dt)
+
+    # temp
+    XR_sol = np.zeros(XR_ref.shape)
+    XR_sol[:dim_x, :] = X_sol
+    XR_sol[-9:, :] = XR_ref[-9:, :]
+    animate_traj(XR_sol, dt)
 
     import ipdb
 
