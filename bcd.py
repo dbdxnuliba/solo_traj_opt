integrate angular momentum to get rotation, bugfix for missing final force qp

bcd.py
@@ -11,7 +11,7 @@ from contact_qp import solve_contact_qp
 from constants import *
 from draw import animate_traj
 from generate_reference import generate_reference
-from utils import extract_X_from_XR
+from utils import extract_X_from_XR, produce_XR_from_X
 
 plt.style.use("seaborn")
 
@@ -33,8 +33,7 @@ if __name__ == "__main__":
         type=strtobool,
         default=1,
     )
-    parser.add_argument(
-        "-n", "--name", help="experiment name", type=str, default=None)
+    parser.add_argument("-n", "--name", help="experiment name", type=str, default=None)
     parser.add_argument(
         "-s", "--save", help="toggle whether to save video", type=strtobool, default=0
     )
@@ -69,10 +68,15 @@ if __name__ == "__main__":
         consensus_arr.append(consensus)
 
         X_prev = X
+    X, info_fqp = solve_force_qp(X, XR_ref, dt)
+    time_arr.append(info_fqp.run_time)
+
+    consensus_arr = np.array(consensus_arr)
+    obj_arr = np.array(obj_arr)
+    time_arr = np.array(time_arr)
 
     print("\ntotal time used in OSQP: {:.3} seconds".format(np.sum(time_arr)))
-    print("total time used in program: {:.3} seconds".format(
-        time.time() - start_time))
+    print("total time used in program: {:.3} seconds".format(time.time() - start_time))
 
     fontsize = 15
     plt.subplot(2, 1, 1)
@@ -95,8 +99,7 @@ if __name__ == "__main__":
     else:
         fname = None
 
-    # temp
-    XR_sol = np.zeros(XR_ref.shape)
-    XR_sol[:dim_x, :] = X
-    XR_sol[-9:, :] = XR_ref[-9:, :]
+    R_init_flat = XR_ref[-9:, 0]
+    R_init = np.reshape(R_init_flat, (3, 3), order="F")
+    XR_sol = produce_XR_from_X(X, dt, R_init)
     animate_traj(XR_sol, dt, fname=fname, display=args.display, repeat=False)

force_qp.py
@@ -4,7 +4,7 @@ import osqp
 
 from constants import *
 from draw import animate_traj
-from utils import extract_state_np, skew_sp, extract_X_from_XR
+from utils import extract_state_np, produce_XR_from_X, skew_sp, extract_X_from_XR
 from generate_reference import generate_reference
 
 
@@ -204,10 +204,10 @@ if __name__ == "__main__":
 
     X_sol, info = solve_force_qp(X_cqp, X_ref, dt)
 
-    # temp
-    XR_sol = np.zeros(XR_ref.shape)
-    XR_sol[:dim_x, :] = X_sol
-    XR_sol[-9:, :] = XR_ref[-9:, :]
+    R_init_flat = XR_ref[-9:, 0]
+    R_init = np.reshape(R_init_flat, (3, 3), order="F")
+    XR_sol = produce_XR_from_X(X_sol, dt, R_init)
+
     animate_traj(XR_sol, dt)
 
     import ipdb

utils.py
@@ -46,7 +46,10 @@ def rot_mat_2d_np(th):
 def rot_mat_np(s, th):
     # normalize s if isn't already normalized
     norm_s = np.linalg.norm(s)
-    assert norm_s != 0.0
+    # assert norm_s != 0.0
+    if norm_s == 0.0:
+        # if zero axis vector given, assume that there is no rotation
+        return np.eye(3)
     s_normalized = s / norm_s
 
     # Rodrigues' rotation formula
@@ -228,6 +231,22 @@ def extract_X_from_XR(XR):
     X = XR[:-9, :]
     assert X.shape[0] == dim_x
     return X
+
+
+# integrate angular velocity to produce rotation matrix and append
+def produce_XR_from_X(X, dt, R_init=np.eye(3)):
+    N = X.shape[1] - 1
+    R_traj = np.zeros((3, 3, N + 1))
+
+    R_traj[:, :, 0] = R_init
+    for t in np.arange(1, N + 1):
+        k_t = X[6:9, t]
+        R_traj[:, :, t] = R_traj[:, :, t - 1] @ rot_mat_np(k_t, dt)
+
+    R_flat_traj = np.reshape(R_traj, (9, N + 1), order="F")
+    XR_sol = np.vstack((X, R_flat_traj))
+
+    return XR_sol
 
 
 # inverse kinematics for the solo 8 robot